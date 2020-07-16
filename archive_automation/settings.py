8eed62dd63850e120342f4594031cf7f88df60ff
Brian Leonard <brian.leonard@mongodb.com>
2020-07-16 11:32:21 -0400

    Switch to auth to use cert

--- archive_automation/settings.py
+++ archive_automation/settings.py
@@ -9,9 +9,12 @@ load_dotenv(dotenv_path)
 # Accessing variables.
 MDB_CONNECTION = os.getenv('MDB_CONNECTION')
 MDB_CONNECTION_ARCHIVE = os.getenv('MDB_CONNECTION_ARCHIVE')
-PEM_FILE = "aoa.pem"
 MDB_DATABASE = os.getenv('MDB_DATABASE')
 MDB_COLLECTION = os.getenv('MDB_COLLECTION')
 NUM_DOCS = os.getenv('NUM_DOCS')
+API_PUBLIC_KEY = os.getenv('API_PUBLIC_KEY')
+API_PRIVATE_KEY = os.getenv('API_PRIVATE_KEY')
+PROJECT_ID = os.getenv('PROJECT_ID')
+CLUSTER_NAME = os.getenv('CLUSTER_NAME')
 
 print("Settings loaded from .env file.")
